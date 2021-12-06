7944b08fac8e84e72e1e93a09856b4ca1222ef1e
syedsuha <syedsuha78@gmail.com>
2021-12-06 13:50:24 +0530

    Added Second Maven project

---
+++ src/main/java/com/demo1company/second_demo_project/App.java
@@ -0,0 +1,13 @@
+package com.demo1company.second_demo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
